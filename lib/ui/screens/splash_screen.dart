a6e532ef22157e46efb9d26a05d0804a2cc9c59a
Abhijit Saha <abhijit0045@gmail.com>
2025-09-08 16:54:04 +0600

    first

---
+++ lib/ui/screens/splash_screen.dart
@@ -0,0 +1,7 @@
+// A splash screen is the very first screen that appears when you open an app.
+// It usually shows the app logo, name, or a loading animation.
+// It stays on screen for a short time while the app loads in the background.
+// After that, it automatically takes you to the main screen of the app (like login or home).
+
+
+

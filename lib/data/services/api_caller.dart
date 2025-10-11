bbb4fcbc728de481693f357f76de0dae305178c4
Butcher-png <abhijit0045@gmail.com>
2025-10-12 01:54:43 +0600

    api

--- lib/data/services/api_caller.dart
+++ lib/data/services/api_caller.dart
@@ -2,12 +2,21 @@ import 'dart:convert';
 import 'dart:nativewrappers/_internal/vm/lib/convert_patch.dart';
 
 import 'package:http/http.dart';
+import 'package:logger/logger.dart';
 
 class ApiCaller {
+  final Logger _logger = Logger();
+
   Future<ApiResponse> getRequest({required String url}) async {
     try {
       Uri uri = Uri.parse(url);
+
+      logRequest(url);
+
       Response response = await get(uri);
+
+      logResponse(url, response);
+
       print(response.statusCode);
       print(uri);
 
@@ -34,6 +43,56 @@ class ApiCaller {
         responseBody: {'error': e.toString()},
       );
     }
+  }
+  Future<ApiResponse> postRequest({required String url, Map<String,dynamic>? body}) async {
+    try {
+      Uri uri = Uri.parse(url);
+
+      logRequest(url,body: body);
+
+      Response response = await post(uri);
+
+      logResponse(url, response);
+
+      print(response.statusCode);
+      print(uri);
+
+      final decodedData = jsonDecode(response.body);
+
+      if (response.statusCode == 200 || response.statusCode==201) {
+        return ApiResponse(
+          isSuccess: true,
+          responseCode: response.statusCode,
+          responseBody: decodedData,
+        );
+      } else {
+        return ApiResponse(
+          isSuccess: false,
+          responseCode: response.statusCode,
+          responseBody: decodedData,
+        );
+      }
+    } catch (e) {
+      // Handle network errors, JSON parsing errors, etc.
+      return ApiResponse(
+        isSuccess: false,
+        responseCode: 500,
+        responseBody: {'error': e.toString()},
+      );
+    }
+  }
+
+  void logRequest(String url, {Map<String, dynamic>? body}) {
+    _logger.i('URL:${url}\n'
+        'Request body: ${body}');
+  }
+
+  void logResponse(String url, Response response) {
+    _logger.i(
+      'URL:${url}\n'
+      'Status Code: ${response.statusCode}\n'
+      'Body: ${response.body}',
+    );
   }
 }
 
